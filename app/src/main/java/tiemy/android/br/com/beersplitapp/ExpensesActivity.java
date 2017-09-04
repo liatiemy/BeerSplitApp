8e3a85d6d35080daa0d5e5fd6f6002bf1a87b933
liatiemy <liazinha.tiemy@gmail.com>
2017-09-03 23:22:51 -0300

    evolução

---
+++ app/src/main/java/tiemy/android/br/com/beersplitapp/ExpensesActivity.java
@@ -0,0 +1,105 @@
+package tiemy.android.br.com.beersplitapp;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tiemy.android.br.com.beersplitapp.adapter.ExpenseAdapter;
+import tiemy.android.br.com.beersplitapp.adapter.LinhaAdapter;
+import tiemy.android.br.com.beersplitapp.api.OnExpenseClickListener;
+import tiemy.android.br.com.beersplitapp.api.OnItemClickListenter;
+import tiemy.android.br.com.beersplitapp.dao.ExpenseDAO;
+import tiemy.android.br.com.beersplitapp.model.Expense;
+import tiemy.android.br.com.beersplitapp.model.RoundRegister;
+
+public class ExpensesActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    private ExpenseAdapter expenseAdapter;
+    static final int REQUEST = 1;
+    static final int REQUEST_UPDATE = 2;
+
+
+    private ExpenseDAO expenseDAO = new ExpenseDAO(this);
+    private Expense expense;
+
+    List<Expense> itens = new ArrayList<Expense>();
+
+    int id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenses);
+
+        Bundle param = getIntent().getExtras();
+        final String id_round = param.getString("id_round");
+        id = Integer.parseInt(id_round);
+        //Toast.makeText(this, "id_round: " + id_round, Toast.LENGTH_SHORT).show();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), ExpenseActivity.class);
+                intent.putExtra("id_round", id_round);
+                startActivityForResult(intent, REQUEST);
+            }
+        });
+
+        recyclerView = (RecyclerView) findViewById(R.id.list);
+
+        expenseAdapter = new ExpenseAdapter(new ArrayList<Expense>(), new OnExpenseClickListener(){
+            @Override
+            public void onItemClick(Expense expense) {
+                Toast.makeText(getApplicationContext(), expense.getNameExpense().toUpperCase(), Toast.LENGTH_SHORT).show();
+                expense = expenseDAO.getByExpense(expense.getNameExpense());
+                Intent intent = new Intent(getApplicationContext(), ExpenseUpdateActivity.class);
+                intent.putExtra("id", expense.getId_round());
+                intent.putExtra("expense", expense.getNameExpense());
+                intent.putExtra("quantity", expense.getQuantity());
+                intent.putExtra("price", expense.getPrice());
+                startActivityForResult(intent, REQUEST_UPDATE);
+            }
+        });
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(expenseAdapter);
+        recyclerView.setHasFixedSize(true);
+
+        //Para inserir as linhas
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
+
+        carregaDados();
+    }
+
+    private void carregaDados(){
+        itens = expenseDAO.getAll(id);
+        expenseAdapter.update(itens);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //if (requestCode == REQUEST) {
+            if (resultCode == RESULT_OK) {
+                //roundRegister.setName(data.getStringExtra("localName").toString());
+                //Toast.makeText(this, "localName: " + roundRegister.getName(), Toast.LENGTH_SHORT).show();
+                carregaDados();
+            }
+        //}
+    }
+}
